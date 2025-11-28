5b988ef27686afb8690f4e69fb3f241aeee3bc9b
kramalmr <akram3107@student.abudzar.sch.id>
2025-11-28 09:09:00 +0700

    edit

--- slicing_ui_almair_05/lib/pages/home.dart
+++ slicing_ui_almair_05/lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:silicing_ui_almair_05/components/appbar.dart';
 import 'package:silicing_ui_almair_05/decorations/set.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,8 +8,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final tinggi = MediaQuery.of(context).size.height;
-    final lebar = MediaQuery.of(context).size.width;
     // return Scaffold(
     //   backgroundColor: ClassName.color5,
     //   body: SingleChildScrollView(
@@ -79,59 +78,8 @@ class HomePage extends StatelessWidget {
     //   ),
     // );
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(24),
-        width: lebar,
-        height: tinggi * 0.3,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [ClassName.color1, ClassName.color3],
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Alamat Kirim", style: ClassName.style6),
-            Row(
-              spacing: 5,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Pondok", style: ClassName.style1),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(60, 30),
-                    padding: EdgeInsets.all(0),
-                    elevation: 0,
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "Utama",
-                    style: GoogleFonts.openSans(
-                      fontSize: 10,
-                      color: Colors.red.shade700,
-                    ),
-                  ),
-                ),
-                Icon(Icons.arrow_forward_ios, color: Colors.white, size: 10),
-                Spacer(),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.chat_bubble, color: Colors.white),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.notifications, color: Colors.white),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.shopping_bag, color: Colors.white),
-                ),
-              ],
-            ),
-          ],
-        ),
+      body: SafeArea(
+        child: SingleChildScrollView(child: Column(children: [AppBarHome()])),
       ),
     );
   }
